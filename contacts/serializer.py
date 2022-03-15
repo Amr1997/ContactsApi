de006b0a0f468145fe672e0d2837cb4ffb805e8d
amr97 <amr_maher1@outlook.com>
2022-03-15 16:16:06 +0200

    created the serializer for contacts

---
+++ contacts/serializer.py
@@ -0,0 +1,7 @@
+from rest_framework import serializers
+from .models import *
+
+class ContactsSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Contact
+        fields =['owner','phone','country_code','first_name', 'last_name' , 'is_fav']
