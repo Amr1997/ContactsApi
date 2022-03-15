796a918296a3a0aef32216ca093111ba00ff22a4
amr97 <amr_maher1@outlook.com>
2022-03-15 16:45:23 +0200

    created the contact view & router

--- contacts/views.py
+++ contacts/views.py
@@ -1,5 +1,18 @@
+
 from rest_framework.viewsets import ModelViewSet
+from rest_framework import permissions
+from .models import *
+from .serializer import *
 # Create your views here.
 
 class ContactsViewSet(ModelViewSet):
-    pass
+    queryset = Contact.objects.all()
+    serializer_class = ContactsSerializer
+    permission_classes=(permissions.IsAuthenticated,)
+    
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
+        
+    def get_queryset(self):
+        return Contact.objects.filter(owner=self.request.user)
+    
