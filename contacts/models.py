aae06b592c740147c44a093c996adc5698c31f1c
amr97 <amr_maher1@outlook.com>
2022-03-17 21:02:37 +0200

    added contact to admin panel

--- contacts/models.py
+++ contacts/models.py
@@ -10,3 +10,7 @@ class Contact(models.Model):
     first_name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=30)
     is_fav = models.BooleanField(default=False)
+    
+    def __str__(self):
+        return self.first_name
+    
